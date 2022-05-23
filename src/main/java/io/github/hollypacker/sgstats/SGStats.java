f05ece9976a1b412c44fed983c91af199869b995
Holly Packer <holly.packer@nitrado.net>
2022-05-23 16:50:38 +0100

    Add files

---
+++ src/main/java/io/github/hollypacker/sgstats/SGStats.java
@@ -0,0 +1,17 @@
+package io.github.hollypacker.sgstats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SGStats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
