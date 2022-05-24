2bf5cefc32987f115415a86b957c927f78333853
Holly Packer <holly.packer@nitrado.net>
2022-05-24 17:23:30 +0100

    Added reload command

---
+++ src/main/java/io/github/hollypacker/sgstats/commands/ReloadCommand.java
@@ -0,0 +1,17 @@
+package io.github.hollypacker.sgstats.commands;
+
+import io.github.hollypacker.sgstats.SGStats;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class ReloadCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
+                             @NotNull String[] args) {
+        SGStats.getInstance().reloadConfig();
+        SGStats.getInstance().sendMessage("reloaded-config", sender);
+        return false;
+    }
+}
